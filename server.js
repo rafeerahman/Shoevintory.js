169903cf1538d06023b48207612f74df2535f6ce
Rafee Rahman <59676643+rafeerahman@users.noreply.github.com>
2022-04-17 22:21:37 -0400

    server modification

--- server.js
+++ server.js
@@ -7,11 +7,7 @@ const express = require('express')
 const app = express();
 
 const path = require('path');
-app.use('/public', express.static(path.join(__dirname, '/public')))
-
-app.get('/', (req, res) => {
-	res.send('<h1>Root</h1>')
-})
+app.use('/', express.static(path.join(__dirname, '/public')))
 
 const port = process.env.PORT || 5000
 app.listen(port, () => {
