e87ef320ec3bee446b2d8fad1a6eba3bb9452982
Rafee Rahman <59676643+rafeerahman@users.noreply.github.com>
2022-05-06 16:43:24 -0400

    unlogging

--- public/app/Shoevintory.js
+++ public/app/Shoevintory.js
@@ -373,15 +373,12 @@ const log = console.log;
             }
             
             for (let i = 1; i < tableProps.columnTitles.length; i++) {
-                console.log('test')
                 const col = this.table.getElementsByClassName(`col${i}`);
-                console.log(col)
                 let col_minWidth = col[0].offsetWidth // column title width
                 let newColCell = col[col.length - 1] // newly added cell width
                 
                 if (newColCell.offsetWidth > col_minWidth) {
                     for (let i = 0; i < col.length; i++) {
-                        console.log(col[i].style)
                         col[i].style.minWidth = `${newColCell.offsetWidth}px`
                     }
                 } else {
